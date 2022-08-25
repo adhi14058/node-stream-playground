32d5a2b1905e645570feef2bdc6365da152243b1
Adhi Narayan B <adhi@one.com>
2022-08-25 15:20:41 +0400

    refactored code

--- server.js
+++ server.js
@@ -1,56 +1,69 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-let publicFolderPath = path.resolve(__dirname, "public");
-let { doSomethingWithStream, getPath, wait } = require("./utils");
+let {
+	doSomethingWithStream,
+	getPath,
+	wait,
+	getStats,
+	print,
+} = require("./utils");
 const app = express();
+let middlewareNumber = 0;
 
 app.use(async (req, res, next) => {
-	console.log("first middleware");
-	let stream = req.stream || req;
-	stream.on("data", (data) => {
-		console.log("\n\nfirst middleware", data);
-	});
-
-	stream.pause();
-	await doSomethingWithStream(stream);
-	stream.resume();
+	let mNum = ++middlewareNumber;
+	print(mNum);
+	await wait(2000);
 	next();
-	let writeStream1 = fs.createWriteStream(getPath("1"));
-	stream.pipe(writeStream1);
 });
 
-app.use((req, res, next) => {
-	console.log("second middleware");
+app.use(async (req, res, next) => {
+	let mNum = ++middlewareNumber;
+	print(mNum);
 	let stream = req.stream || req;
-	stream.on("data", (data) => {
-		console.log("\n\nsecond middleware", data);
-	});
-	let writeStream2 = fs.createWriteStream(getPath("2"));
-	stream.pipe(writeStream2);
+	stream.on("data", (data) => print(mNum, data));
+	// stream.pause();
+	// await doSomethingWithStream(stream);
+	// stream.resume();
+
+	let writeStream = fs.createWriteStream(getPath(`${mNum}`));
+	stream.pipe(writeStream);
 	next();
 });
 
 app.use((req, res, next) => {
-	console.log("third middleware");
+	let mNum = ++middlewareNumber;
+	print(mNum);
+
 	let stream = req.stream || req;
-	stream.on("data", (data) => {
-		console.log("\n\nthird middleware", data);
-	});
-	let writeStream3 = fs.createWriteStream(getPath("3"));
+	stream.on("data", (data) => print(mNum, data));
+
+	let writeStream = fs.createWriteStream(getPath(`${mNum}`));
+	stream.pipe(writeStream);
+	next();
+});
+
+app.use((req, res, next) => {
+	let mNum = ++middlewareNumber;
+	print(mNum);
+
+	let stream = req.stream || req;
+	stream.on("data", (data) => print(mNum, data));
+
+	let writeStream3 = fs.createWriteStream(getPath(`${mNum}`));
 	stream.pipe(writeStream3);
 	next();
 });
 
 app.use(async (req, res, next) => {
+	let mNum = ++middlewareNumber;
+	print(mNum);
+
 	await wait(3000);
-	let publicFolder = fs.readdirSync(publicFolderPath);
-	let result = {};
-	for (let fileName of publicFolder) {
-		let filePath = path.resolve(publicFolderPath, fileName);
-		result[fileName] = fs.statSync(filePath).size;
-		if (fileName !== "fileTobeUploaded.txt") fs.unlink(filePath, () => {});
-	}
+
+	let result = getStats();
+	middlewareNumber = 0;
 	res.send({ result });
 });
 
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+let publicFolderPath = path.resolve(__dirname, "public");
+
 function doSomethingWithStream(stream) {
 	return new Promise((res, rej) => {
 		let writeStream1a = fs.createWriteStream(getPath("doSomethingWithStream"));
@@ -21,8 +23,24 @@ function wait(ms) {
 	return new Promise((res) => setTimeout(res, ms));
 }
 
+function getStats() {
+	let publicFolder = fs.readdirSync(publicFolderPath);
+	let result = {};
+	for (let fileName of publicFolder) {
+		let filePath = path.resolve(publicFolderPath, fileName);
+		result[fileName] = fs.statSync(filePath).size;
+		if (fileName !== "fileTobeUploaded.txt") fs.unlink(filePath, () => {});
+	}
+	return result;
+}
+
+function print(middlewareNumber, data) {
+	console.log(`\nmiddleware-${middlewareNumber}`, data ? data : "");
+}
 module.exports = {
 	doSomethingWithStream,
 	getPath,
 	wait,
+	getStats,
+	print,
 };
